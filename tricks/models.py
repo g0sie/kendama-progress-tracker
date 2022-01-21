276ce016bd0470a50e6111dc2ed5a3982cd3bd40
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-01-21 12:44:33 +0100

    add UserTrick model

--- tricks/models.py
+++ tricks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 from common.models import Timestamped
 
@@ -23,3 +24,11 @@ class Trick(Timestamped):
 
     def __str__(self):
         return self.name
+
+
+class UserTrick(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name="tricks")
+    trick = models.ForeignKey('Trick', on_delete=models.CASCADE, related_name="users")
+    land_count = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(100)])
+    added = models.DateTimeField(auto_now_add=True)
+    passed = models.DateTimeField(blank=True, null=True)
