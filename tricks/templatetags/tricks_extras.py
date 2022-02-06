3e64ef24c6ba9cace7176a4ed2b56b598566d115
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-02-06 16:55:11 +0100

    add progress_color tag

--- tricks/templatetags/tricks_extras.py
+++ tricks/templatetags/tricks_extras.py
@@ -4,8 +4,8 @@ register = template.Library()
 
 
 @register.simple_tag
-def color(rank: int) -> str:
-    """returns css color"""
+def card_color(rank: int) -> str:
+    """returns css color for card"""
     colors = {
         1: "bg-secondary text-white",
         2: "bg-dark text-white",
@@ -13,5 +13,19 @@ def color(rank: int) -> str:
         4: "bg-danger text-white",
         5: "bg-warning text-dark",
         6: "bg-info text-dark",
+    }
+    return colors[rank]
+
+
+@register.simple_tag
+def progress_color(rank: int) -> str:
+    """returns css color for progress bar"""
+    colors = {
+        1: "progress-bar-striped bg-dark text-white",
+        2: "progress-bar-striped bg-secondary text-white",
+        3: "progress-bar-striped bg-secondary text-white",
+        4: "progress-bar-striped bg-secondary text-white",
+        5: "progress-bar-striped bg-secondary text-white",
+        6: "progress-bar-striped text-white",
     }
     return colors[rank]
