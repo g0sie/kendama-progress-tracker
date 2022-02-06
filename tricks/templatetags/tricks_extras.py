5060f55600dd3bcf26668a9fc50566257ec85925
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-02-06 17:48:28 +0100

    add progress_bar tag

--- tricks/templatetags/tricks_extras.py
+++ tricks/templatetags/tricks_extras.py
@@ -1,11 +1,12 @@
 from django import template
+from django.utils.html import format_html
 
 register = template.Library()
 
 
 @register.simple_tag
 def card_color(rank: int) -> str:
-    """returns css color for card"""
+    """returns bg color for card"""
     colors = {
         1: "bg-secondary text-white",
         2: "bg-dark text-white",
@@ -17,15 +18,32 @@ def card_color(rank: int) -> str:
     return colors[rank]
 
 
-@register.simple_tag
 def progress_color(rank: int) -> str:
-    """returns css color for progress bar"""
+    """returns bg color for progress bar"""
     colors = {
         1: "progress-bar-striped bg-dark text-white",
         2: "progress-bar-striped bg-secondary text-white",
         3: "progress-bar-striped bg-secondary text-white",
         4: "progress-bar-striped bg-secondary text-white",
-        5: "progress-bar-striped bg-secondary text-white",
-        6: "progress-bar-striped text-white",
     }
     return colors[rank]
+
+
+@register.simple_tag
+def progress_bar(rank: int, land_count: int):
+    """returns html progress bar"""
+    max_value = {
+        1: 10,
+        2: 25,
+        3: 50,
+        4: 100,
+    }
+    land_count = min(land_count, max_value[rank])
+    percentage = land_count / max_value[rank] * 100
+    html_str = '<div class="progress">'
+    html_str += f'<div class="progress-bar {progress_color(rank)}" role="progressbar" '
+    html_str += f'aria-valuenow="{land_count}" style="width: {percentage}%" '
+    html_str += f'aria-valuemin="0" aria-valuemax="{max_value[rank]}">'
+    html_str += f'{land_count}</div>'
+    html_str += '</div>'
+    return format_html(html_str)
