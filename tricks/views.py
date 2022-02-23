0a39c53b8e0f6f8809633ee89f3879a104d5d362
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-02-23 19:29:42 +0100

    delete base tricks

--- tricks/views.py
+++ tricks/views.py
@@ -20,7 +20,7 @@ def user_tricks(request):
                 rank_up_trick(key)
             return HttpResponseRedirect(reverse('tricks:user_tricks'))
         user_trick_pairs = UserTrick.objects.filter(user=request.user)\
-            .select_related("trick").prefetch_related("trick__tutorials", "trick__base_tricks")
+            .select_related("trick").prefetch_related("trick__tutorials")
         return render(request, "tricks/user_tricks.html", {'user_trick_pairs': user_trick_pairs})
 
 
