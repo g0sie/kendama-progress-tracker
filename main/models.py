6d427cfe93d91c25c93eefeaf1fda173829537b1
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-02-25 15:34:56 +0100

    add ken number to header

--- main/models.py
+++ main/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField("auth.User", on_delete=models.CASCADE)
+    user = models.OneToOneField("auth.User", on_delete=models.CASCADE, related_name="profile")
     kens = models.PositiveIntegerField(default=0)
