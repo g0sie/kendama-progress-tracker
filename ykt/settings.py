a940ce93075192983d5bfbd0d05bcf59b647a2cc
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-07-11 00:40:55 +0200

    debug false

--- ykt/settings.py
+++ ykt/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = [
