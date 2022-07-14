14fc2f6498de1dfd13d571a184b896297e2e5d75
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-07-14 17:43:37 +0200

    filter tutorials without trick in admin panel

--- tutorials/admin.py
+++ tutorials/admin.py
@@ -1,12 +1,30 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from .models import Tutorial, Author, Playlist
+
+
+class NullTrickFilter(admin.SimpleListFilter):
+    title = _('to add')
+
+    parameter_name = 'trick'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('null', _('tutorials to add')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'null':
+            return queryset.filter(
+                trick__name='null'
+            )
 
 
 @admin.register(Tutorial)
 class TutorialAdmin(admin.ModelAdmin):
     list_display = ['trick', 'author', 'created', 'modified']
-    list_filter = ['author']
+    list_filter = ['author', NullTrickFilter]
 
 
 @admin.register(Author)
