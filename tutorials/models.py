95d47c5bc9911cbcf5ab52f78b2ff167d4d514bc
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-07-14 16:20:03 +0200

    add Author model

--- tutorials/models.py
+++ tutorials/models.py
@@ -7,5 +7,10 @@ class Tutorial(Timestamped):
     yt_title = models.CharField(max_length=100, null=True)
     trick = models.ForeignKey(
         "tricks.Trick", on_delete=models.CASCADE, related_name="tutorials")
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        'Author', on_delete=models.CASCADE, related_name='tutorials')
     url = models.URLField()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=15)
