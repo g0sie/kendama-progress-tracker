b2715bc4471655ab61f39a3c63cc8f8b30b60fab
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-07-14 21:47:58 +0200

    add add_tutorials view

--- tutorials/views.py
+++ tutorials/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import reverse
+from django.contrib.auth.decorators import user_passes_test
+from django.core.management import call_command
 
-# Create your views here.
+
+@user_passes_test(lambda u: u.is_superuser)
+def add_tutorials(request, number: int):
+    call_command('gettutorials', max=number)
+    return HttpResponseRedirect(reverse('admin:tutorials_tutorial_changelist'))
