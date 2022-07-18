278835314e87b65d5e71df90817906ed56c19af9
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-07-18 16:22:30 +0200

    add add_from list page navigation

--- tricks/views.py
+++ tricks/views.py
@@ -96,12 +96,19 @@ def add_from_list(request):
         if add_key not in UserTrick.objects.filter(user=request.user).values_list("trick__id", flat=True):
             trick = Trick.objects.get(id=add_key)
             UserTrick.objects.create(user=request.user, trick=trick)
+
+    tricks_number = request.GET.get('tricks', 12)
     # display only the tricks the user doesn't have
     user_tricks_ids = UserTrick.objects.filter(
         user=request.user).values('trick__id')
     tricks = Trick.objects.filter(official=True).exclude(
         id__in=user_tricks_ids).prefetch_related("tutorials")
-    return render(request, 'tricks/add_from_list.html', {'tricks': tricks})
+
+    paginator = Paginator(tricks, tricks_number)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'tricks/add_from_list.html', {'tricks': page_obj, 'tricks_number': tricks_number})
 
 
 def get_random_user_trick(user):
