f0a0fd6a2443ae3783e635df9688398734018d8b
Małgorzata Derkacz <malgorzataderkacz3@gmail.com>
2022-07-29 21:59:42 +0200

    test register post valid

--- register/tests/test_views.py
+++ register/tests/test_views.py
@@ -1,5 +1,8 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+from main.models import UserProfile
 
 
 class TestViews(TestCase):
@@ -12,3 +15,18 @@ class TestViews(TestCase):
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'accounts/register.html')
+
+    def test_register_POST_valid(self):
+        response = self.client.post(self.register_url, {
+            'username': 'user123',
+            'email': 'email123@op.pl',
+            'password1': 'p4ssword123',
+            'password2': 'p4ssword123'
+        })
+        user = User.objects.get(username='user123')
+        userprofile = UserProfile.objects.get(user=user)
+
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url, reverse('index'))
+        self.assertEquals(User.objects.all().count(), 1)
+        self.assertEquals(UserProfile.objects.all().count(), 1)
